277d3fd524af8e0fcc33abb9e567c230e6d1e2b5
Willian Paixao <willian.paixaoo@gmail.com>
2014-01-31 23:46:02 -0500

    JSONRPC.py added.
    This new class is the lowest level so far. It's the responsible for handling
    HTTP requests and return the JSON response.
    
    It can ask for all or specifics permissions, allowing a fine control of XBMC
    remote calls.
    A ping method checks for connectivity. And version method return the XMBC
    system's version.
    
    The documentation is coming, I promise. For now, the code is very well written.

---
+++ JSONRPC.py
@@ -0,0 +1,45 @@
+"""
+Created on 31 January 2014
+"""
+
+import requests
+import json
+
+class JSONRPC(object):
+    """
+    :Author:  Willian Paixao <willian.paixaoo@gmail.com>
+    :Version: 0.01
+    """
+
+    def __init__(self, object):
+        self.url = object["url"]
+        self.headers = object["headers"]
+        self.payload = object["payload"]
+
+    def get_permission(self): return self.post(method="JSONRPC.Permission")
+
+    def has_permission(self, permission):
+        p = self.get_permission()
+        return p["result"][permission]
+
+    def ping(self):
+        p = self.post("JSONRPC.Ping")
+        if p["result"] == u'pong':
+            return True
+        else:
+            return False
+
+    def post(self, method, params={}):
+        self.payload["method"] = method
+        if params:
+            self.payload["params"] = params
+        r = requests.post(self.url, data=json.dumps(self.payload),
+                headers=self.headers)
+        return r.json()
+
+    def version(self):
+        r = self.post("JSONRPC.Version")
+        return r["result"]["version"]
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 textwidth=80
+
