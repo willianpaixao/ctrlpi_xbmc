5add42bbe0164f48ad2014a0d03950d55d3bfc70
Willian Paixao <willian.paixaoo@gmail.com>
2014-02-18 01:35:54 -0500

    Media classes refactored.
    As the last commit, I'm moving and grouping some classes for the sake of
    organization. Nothing big deal, I will also add some useless documentation in he
    next commit.

--- ctrlpi/media.py
+++ ctrlpi/media.py
@@ -1,4 +1,37 @@
-from client import System
+from client import JSONRPC, System
+
+class VideoLibrary(JSONRPC):
+    """
+    .. todo:: Implement failing procedure.
+
+    :Author:  Willian Paixao <willian.paixaoo@gmail.com>
+    :Version: 0.01
+    """
+
+    def __init__(self, object):
+        super(VideoLibrary, self).__init__(object)
+
+    def clean(self):
+        if self.has_permission(permission="RemoveData"):
+            r = self.post(method="VideoLibrary.Clean")
+            return self.result_is_ok(r)
+        else:
+
+            return False
+
+    def export(self):
+        if self.has_permission(permission="WriteFile"):
+            r = self.post(method="VideoLibrary.Export")
+            return self.result_is_ok(r)
+        else:
+            return False
+
+    def scan(self):
+        if self.has_permission(permission="UpdateData"):
+            r = self.post(method="VideoLibrary.Scan")
+            return self.result_is_ok(r)
+        else:
+            return False
 
 class Playlist(System):
     """
