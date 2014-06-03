697c95c51e8c9807a0c4154a36ed8ed1e476c8de
Willian Paixao <willian.paixaoo@gmail.com>
2014-06-03 12:45:19 -0400

    XBMC framework update.
    Now using Raspbmc with XBMC 13.0 "Gotham". See project changelog for further
    information.
    
    [1] http://www.raspbmc.com/
    [2] http://xbmc.org/

--- test/TestRequestHandler.py
+++ test/TestRequestHandler.py
@@ -19,7 +19,7 @@ class TestRequestHandler(unittest.TestCase):
 
     def test_post(self):
         data["headers"] = {"content-type": "application/json"}
-        data["url"] = "http://192.168.1.39:8080/jsonrpc"
+        data["url"] = "http://192.168.1.4:8080/jsonrpc"
         data["payload"] = {"jsonrpc": "2.0", "id": 1}
         data["payload"]["method"] = "JSONRPC.Ping"
 
@@ -29,7 +29,7 @@ class TestRequestHandler(unittest.TestCase):
 
         data["payload"]["method"] = "JSONRPC.Version"
         s = {u'jsonrpc': u'2.0', u'id': 1, u'result': {u'version': {u'major': 6,
-            u'minor': 0, u'patch': 3}}}
+            u'minor': 14, u'patch': 3}}}
         t = r.post(data["payload"])
         self.assertEquals(t, s)
 
