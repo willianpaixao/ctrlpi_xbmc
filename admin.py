0634fbea2633628dc36a6c9e2bc7912acd376b47
Willian Paixao <willian.paixaoo@gmail.com>
2014-07-25 02:05:24 -0400

    It's alive!
    Yesterday I added another repository, called dashboard. The purpose is to
    initiate the website that will control all the clients
    
    This commit is the back-end adaptation, which includes refactoring in the model
    file. Now it imports Node class and gives real-time data from the clients.
    
    So awesome! I want to high-five myself :)

--- admin.py
+++ admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from ctrlpi.models import Playlist, Video, XBMCNode
+from ctrlpi_xbmc.models import Playlist, Video, Node
 
 admin.site.register(Playlist)
 admin.site.register(Video)
-admin.site.register(XBMCNode)
+admin.site.register(Node)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 textwidth=80
 
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from ctrlpi_xbmc.client import JSONRPC
+
 class Playlist(models.Model):
     title = models.CharField(max_length=512)
     fanart_url = models.CharField(max_length=256, blank=True)
@@ -21,12 +23,34 @@ class Video(models.Model):
     fanart_url = models.CharField(max_length=256, blank=True)
     thumbnail_url = models.CharField(max_length=256, blank=True)
 
-class XBMCNode(models.Model):
+class Node(models.Model):
     name = models.CharField(max_length=512)
     address = models.GenericIPAddressField()
     port = models.IntegerField()
+    mac = models.CharField(max_length=32)
     username = models.CharField(max_length=512, blank=True)
     password = models.CharField(max_length=512, blank=True)
+
+    def __unicode__(self):
+            return self.name
+
+    def get_header(self):
+        data = {}
+        data["headers"] = {"content-type": "application/json"}
+        data["payload"] = {"jsonrpc": "2.0", "id": 1}
+        data["url"] = "http://" + self.address + ":" + str(self.port) + "/jsonrpc"
+        return data
+        
+    def ping(self):
+        r = JSONRPC(self.get_header())
+        s = r.ping()
+        return s
+
+    def task(self):
+        return "fake"
+
+    def uptime(self):
+        return "fake"
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 textwidth=80
 
